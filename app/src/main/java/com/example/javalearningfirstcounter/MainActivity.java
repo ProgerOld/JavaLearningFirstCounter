02c290b6d165f89914c0cc8e4f76bdcab2d450f8
Konstantin <elagink@mail.ru>
2023-04-07 17:32:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/javalearningfirstcounter/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.javalearningfirstcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
