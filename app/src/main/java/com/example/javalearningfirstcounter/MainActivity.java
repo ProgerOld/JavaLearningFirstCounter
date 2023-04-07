e99952a10c14c65bee5e7133ee996cb6bf30ff71
Konstantin <elagink@mail.ru>
2023-04-07 19:41:07 +0500

    Add

--- app/src/main/java/com/example/javalearningfirstcounter/MainActivity.java
+++ app/src/main/java/com/example/javalearningfirstcounter/MainActivity.java
@@ -3,16 +3,35 @@ package com.example.javalearningfirstcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textCount;
+    private Button button;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textCount = findViewById(R.id.textCount);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(listener);
+
     }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            count++;
+            textCount.setText(Integer.toString(count));
+
+        }
+    };
+
 }
